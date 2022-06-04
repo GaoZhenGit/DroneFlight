aecdcf7ae4f09affce3270d1ee6d454d9e740841
GaoZhenGit <912815282@qq.com>
2022-06-04 16:09:31 +0800

    reconstruct the UI and state checking

--- app/src/main/java/hk/hku/flight/DroneApplication.java
+++ app/src/main/java/hk/hku/flight/DroneApplication.java
@@ -3,6 +3,8 @@ package hk.hku.flight;
 import android.app.Application;
 import android.content.Context;
 
+import androidx.multidex.MultiDex;
+
 import com.secneo.sdk.Helper;
 
 import dji.sdk.base.BaseProduct;
@@ -68,6 +70,7 @@ public class DroneApplication extends Application {
     @Override
     protected void attachBaseContext(Context paramContext) {
         super.attachBaseContext(paramContext);
+        MultiDex.install(this);
         com.secneo.sdk.Helper.install(this);
         app = this;
     }
--- app/src/main/java/hk/hku/flight/MainActivity.java
+++ app/src/main/java/hk/hku/flight/MainActivity.java
@@ -16,12 +16,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -32,31 +38,35 @@ import dji.common.airlink.SignalQualityCallback;
 import dji.common.battery.BatteryState;
 import dji.common.error.DJIError;
 import dji.common.error.DJISDKError;
+import dji.common.util.CommonCallbacks;
 import dji.sdk.airlink.AirLink;
 import dji.sdk.base.BaseComponent;
 import dji.sdk.base.BaseProduct;
 import dji.sdk.battery.Battery;
+import dji.sdk.products.Aircraft;
 import dji.sdk.sdkmanager.DJISDKInitEvent;
 import dji.sdk.sdkmanager.DJISDKManager;
+import dji.sdk.sdkmanager.LDMModule;
+import dji.sdk.sdkmanager.LDMModuleType;
 import hk.hku.flight.util.ThreadManager;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private final Handler mHandler = new Handler(Looper.getMainLooper());
-    private static final String[] REQUIRED_PERMISSION_LIST = new String[]{
-            Manifest.permission.VIBRATE,
-            Manifest.permission.INTERNET,
-            Manifest.permission.ACCESS_WIFI_STATE,
-            Manifest.permission.WAKE_LOCK,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_NETWORK_STATE,
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.CHANGE_WIFI_STATE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE,
-            Manifest.permission.BLUETOOTH,
-            Manifest.permission.BLUETOOTH_ADMIN,
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.READ_PHONE_STATE,
+    private static final String[] REQUIRED_PERMISSION_LIST = new String[] {
+            Manifest.permission.VIBRATE, // Gimbal rotation
+            Manifest.permission.INTERNET, // API requests
+            Manifest.permission.ACCESS_WIFI_STATE, // WIFI connected products
+            Manifest.permission.ACCESS_COARSE_LOCATION, // Maps
+            Manifest.permission.ACCESS_NETWORK_STATE, // WIFI connected products
+            Manifest.permission.ACCESS_FINE_LOCATION, // Maps
+            Manifest.permission.CHANGE_WIFI_STATE, // Changing between WIFI and USB connection
+            Manifest.permission.WRITE_EXTERNAL_STORAGE, // Log files
+            Manifest.permission.BLUETOOTH, // Bluetooth connected products
+            Manifest.permission.BLUETOOTH_ADMIN, // Bluetooth connected products
+            Manifest.permission.READ_EXTERNAL_STORAGE, // Log files
+            Manifest.permission.READ_PHONE_STATE, // Device UUID accessed upon registration
+            Manifest.permission.RECORD_AUDIO // Speaker accessory
     };
     private List<String> missingPermission = new ArrayList<>();
     private AtomicBoolean isRegistrationInProgress = new AtomicBoolean(false);
@@ -65,71 +75,95 @@ public class MainActivity extends AppCompatActivity {
     private final BroadcastReceiver mConnectionReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            Log.i(TAG, "on Device state change, try re-register");
-            BaseProduct product = DJISDKManager.getInstance().getProduct();
-            if (product == null) {
-                Log.i(TAG, "connection loss");
-                return;
-            }
-            AirLink airLink = product.getAirLink();
-            if (airLink == null) {
-                Log.i(TAG, "airLink connection loss");
-            } else {
-                airLink.setUplinkSignalQualityCallback(null);
-                airLink.setDownlinkSignalQualityCallback(i -> {
-                    Log.i(TAG, "Down link SignalQuality:" + i);
-                    Intent intent1 = new Intent(FLAG_DOWN_SIGNAL_QUANTITY);
-                    intent1.putExtra(FLAG_DOWN_SIGNAL_QUANTITY, i);
-                    sendBroadcast(intent1);
-                });
-                airLink.setDownlinkSignalQualityCallback(null);
-                airLink.setUplinkSignalQualityCallback(i -> {
-                    Log.i(TAG, "Up link SignalQuality:" + i);
-                    Intent intent12 = new Intent(FLAG_UP_SIGNAL_QUANTITY);
-                    intent12.putExtra(FLAG_UP_SIGNAL_QUANTITY, i);
-                    sendBroadcast(intent12);
-                });
-            }
-            Battery battery = product.getBattery();
-            if (battery == null) {
-                Log.i(TAG, "battery connection loss");
-            } else {
-                battery.setStateCallback(null);
-                battery.setStateCallback(batteryState -> {
-                    int percentage = batteryState.getChargeRemainingInPercent();
-                    Log.i(TAG, "battery percentage:" + percentage);
-                    Intent intent13 = new Intent(FLAG_BATTERY_PERCENTAGE);
-                    intent13.putExtra(FLAG_BATTERY_PERCENTAGE, percentage);
-                    sendBroadcast(intent13);
-                });
-            }
+            checkRegister();
+            checkConnect();
+//            Log.i(TAG, "on Device state change, try re-register");
+//            BaseProduct product = DJISDKManager.getInstance().getProduct();
+//            if (product == null) {
+//                Log.i(TAG, "connection loss");
+//                return;
+//            }
+//            AirLink airLink = product.getAirLink();
+//            if (airLink == null) {
+//                Log.i(TAG, "airLink connection loss");
+//            } else {
+//                airLink.setUplinkSignalQualityCallback(null);
+//                airLink.setDownlinkSignalQualityCallback(i -> {
+//                    Log.i(TAG, "Down link SignalQuality:" + i);
+//                    Intent intent1 = new Intent(FLAG_DOWN_SIGNAL_QUANTITY);
+//                    intent1.putExtra(FLAG_DOWN_SIGNAL_QUANTITY, i);
+//                    sendBroadcast(intent1);
+//                });
+//                airLink.setDownlinkSignalQualityCallback(null);
+//                airLink.setUplinkSignalQualityCallback(i -> {
+//                    Log.i(TAG, "Up link SignalQuality:" + i);
+//                    Intent intent12 = new Intent(FLAG_UP_SIGNAL_QUANTITY);
+//                    intent12.putExtra(FLAG_UP_SIGNAL_QUANTITY, i);
+//                    sendBroadcast(intent12);
+//                });
+//            }
+//            Battery battery = product.getBattery();
+//            if (battery == null) {
+//                Log.i(TAG, "battery connection loss");
+//            } else {
+//                battery.setStateCallback(null);
+//                battery.setStateCallback(batteryState -> {
+//                    int percentage = batteryState.getChargeRemainingInPercent();
+//                    Log.i(TAG, "battery percentage:" + percentage);
+//                    Intent intent13 = new Intent(FLAG_BATTERY_PERCENTAGE);
+//                    intent13.putExtra(FLAG_BATTERY_PERCENTAGE, percentage);
+//                    sendBroadcast(intent13);
+//                });
+//            }
         }
     };
 
     @Override
+    protected void onNewIntent(@NonNull Intent intent) {
+        super.onNewIntent(intent);
+        String action = intent.getAction();
+        if (UsbManager.ACTION_USB_ACCESSORY_ATTACHED.equals(action)) {
+            Intent attachedIntent = new Intent();
+            attachedIntent.setAction(DJISDKManager.USB_ACCESSORY_ATTACHED);
+            sendBroadcast(attachedIntent);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//这行代码一定要在setContentView之前，不然会闪退
         setContentView(R.layout.activity_main);
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         initView();
         setInfoCallback();
     }
 
     private void initView() {
-        findViewById(R.id.btn_init).setOnClickListener(view -> {
-            if (isRegistrationInProgress.compareAndSet(false, true)) {
-                checkAndRequestPermissions();
-            } else {
-                mConnectionReceiver.onReceive(null, null);
-            }
-        });
-
-        findViewById(R.id.btn_open).setOnClickListener(v -> {
+        checkPermission();
+        findViewById(R.id.btn_permission).setOnClickListener(v -> requestNeedPermission());
+        if (missingPermission.isEmpty()) {
+            startSDKRegistration();
+        }
+        startSDKRegistration();
+        findViewById(R.id.btn_register).setOnClickListener(v -> {
+            checkPermission();
             if (!missingPermission.isEmpty()) {
-                Log.i(TAG, "lack of permission:" + missingPermission);
-                showToast("lack of permission:" + missingPermission);
+                Toast.makeText(getBaseContext(), "Please click \"Permission\" first.", Toast.LENGTH_SHORT).show();
                 return;
             }
-
+            if (!isRegistrationInProgress.get()) {
+                startSDKRegistration();
+            }
+        });
+        checkConnect();
+        findViewById(R.id.btn_drone).setOnClickListener(v -> checkConnect());
+        findViewById(R.id.btn_RC).setOnClickListener(v -> checkConnect());
+        findViewById(R.id.btn_start_fly).setOnClickListener(v -> {
+            if (!v.isEnabled()) {
+                return;
+            }
             Intent intent = new Intent(MainActivity.this, FlightActivity.class);
             startActivity(intent);
         });
@@ -137,12 +171,14 @@ public class MainActivity extends AppCompatActivity {
 
     private void startSDKRegistration() {
         showToast("registering...");
-        ThreadManager.getInstance().submit(() -> DJISDKManager.getInstance().registerApp(getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
+        AsyncTask.execute(() -> DJISDKManager.getInstance().registerApp(getApplicationContext(), new DJISDKManager.SDKManagerCallback() {
             @Override
             public void onRegister(DJIError djiError) {
                 if (djiError == DJISDKError.REGISTRATION_SUCCESS) {
                     showToast("Register Success");
                     DJISDKManager.getInstance().startConnectionToProduct();
+                    isRegistrationInProgress.set(true);
+                    checkRegister();
                 } else {
                     showToast("Register sdk fails, please check the bundle id and network connection!");
                 }
@@ -174,7 +210,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onComponentChange(BaseProduct.ComponentKey componentKey, BaseComponent oldComponent,
                                           BaseComponent newComponent) {
-
+                Log.i(TAG, "onComponentChange: " + newComponent);
                 if (newComponent != null) {
                     newComponent.setComponentListener(new BaseComponent.ComponentListener() {
 
@@ -204,8 +240,7 @@ public class MainActivity extends AppCompatActivity {
         }));
     }
 
-    private void checkAndRequestPermissions() {
-        // Check for permissions
+    private void checkPermission() {
         for (String eachPermission : REQUIRED_PERMISSION_LIST) {
             if (ContextCompat.checkSelfPermission(this, eachPermission) != PackageManager.PERMISSION_GRANTED) {
                 missingPermission.add(eachPermission);
@@ -213,14 +248,64 @@ public class MainActivity extends AppCompatActivity {
         }
         // Request for missing permissions
         if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ((ImageView)findViewById(R.id.ic_permission)).setImageResource(R.drawable.state_ok);
+        } else {
+            ((ImageView)findViewById(R.id.ic_permission)).setImageResource(R.drawable.state_error);
+        }
+    }
+    private void requestNeedPermission() {
+        checkPermission();
+        if (missingPermission.isEmpty()) {
+            return;
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             showToast("Need to grant the permissions!");
             ActivityCompat.requestPermissions(this,
                     missingPermission.toArray(new String[missingPermission.size()]),
                     REQUEST_PERMISSION_CODE);
         }
-
+    }
+    private void checkRegister() {
+        if (isRegistrationInProgress.get()) {
+            ((ImageView)findViewById(R.id.ic_register)).setImageResource(R.drawable.state_ok);
+        } else {
+            ((ImageView)findViewById(R.id.ic_register)).setImageResource(R.drawable.state_error);
+        }
+    }
+    private void checkConnect() {
+        Log.i(TAG, "checkConnect");
+        BaseProduct product = DroneApplication.getProductInstance();
+        if (product == null) {
+            Log.i(TAG, "no product connect");
+            ((ImageView)findViewById(R.id.ic_drone)).setImageResource(R.drawable.state_error);
+            ((ImageView)findViewById(R.id.ic_RC)).setImageResource(R.drawable.state_error);
+            findViewById(R.id.btn_start_fly).setEnabled(false);
+            return;
+        }
+        if (product.isConnected()) {
+            Log.i(TAG, "product connect");
+            ((ImageView)findViewById(R.id.ic_drone)).setImageResource(R.drawable.state_ok);
+            ((ImageView)findViewById(R.id.ic_RC)).setImageResource(R.drawable.state_ok);
+            findViewById(R.id.btn_start_fly).setEnabled(true);
+            return;
+        }
+        if (product instanceof Aircraft) {
+            Aircraft aircraft = (Aircraft) product;
+            if (aircraft.getRemoteController() != null && aircraft.getRemoteController().isConnected()) {
+                Log.i(TAG, "only RC connect");
+                ((ImageView)findViewById(R.id.ic_drone)).setImageResource(R.drawable.state_error);
+                ((ImageView)findViewById(R.id.ic_RC)).setImageResource(R.drawable.state_ok);
+            } else {
+                Log.i(TAG, "no product connect 2");
+                ((ImageView)findViewById(R.id.ic_drone)).setImageResource(R.drawable.state_error);
+                ((ImageView)findViewById(R.id.ic_RC)).setImageResource(R.drawable.state_error);
+            }
+        } else  {
+            Log.i(TAG, "no product connect 3");
+            ((ImageView)findViewById(R.id.ic_drone)).setImageResource(R.drawable.state_error);
+            ((ImageView)findViewById(R.id.ic_RC)).setImageResource(R.drawable.state_error);
+        }
+        findViewById(R.id.btn_start_fly).setEnabled(false);
     }
 
     /**
@@ -240,11 +325,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }
         // If there is enough permission, we will start the registration
-        if (missingPermission.isEmpty()) {
-            startSDKRegistration();
-        } else {
-            showToast("Missing permissions!!!");
-        }
+        checkPermission();
     }
 
     private void setInfoCallback() {
